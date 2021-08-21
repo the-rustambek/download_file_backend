8f8fa9f119214541f1469a54a325b563fb5954d2
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-08-21 16:38:26 +0500

    add file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const server = express()
 const path = require('path')
+const expressFileUpload = require("express-fileupload")
 
 // const ejsLint = require('ejs-lint');
 
@@ -20,26 +21,35 @@ server.use(
 
 server.use(express.static(path.join(__dirname,"public")))
 
-server.get("/",(req,res) =>{
+server.get(["/","/index.js"],(req,res) =>{
     res.render("index",{
         data,
     })
 })
 
-server.post("/",(req,res) =>{
-    data.push({ 
-        id: data.length + 1, 
-        name: req.body.name, 
-        date: req.body.date,
+server.post("/",expressFileUpload(),(req,res) =>{
+    // data.push({ 
+    //     id: data.length + 1, 
+    //     name: req.body.name, 
+    //     date: req.body.date,
 
-    })
+    // })
+    console.log(req.body);
+    data.push(req.body)
     res.redirect("/")
     // console.log(req.body);
 })
 
-server.get("/delete/:id", (req,res) =>{
-    let index =data.findIndex((e) =>( e.id == req.params.id))
-    data.splice(index,1)
-    res.redirect("/")
+// server.get("/delete/:id", (req,res) =>{
+//     let index =data.findIndex((e) =>( e.id == req.params.id))
+//     data.splice(index,1)
+//     res.redirect("/")
+// })
+
+server.get("/admin", (req,res) =>{
+    
+    res.render("index",{
+        data,
+    })
 })
 
