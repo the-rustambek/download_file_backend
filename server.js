0f616d57c10a29f014c067384cf133ce13a47962
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-08-22 01:01:36 +0500

    add file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const expressFileUpload = require("express-fileupload")
 
 // const ejsLint = require('ejs-lint');
 
-server.listen(5050, () => console.log("Server ready at 8090"))
+server.listen(3333, () => console.log("Server ready at 8090"))
 
 server.set("view engine", "ejs")
 
@@ -13,33 +13,60 @@ const data = []
 
 server.use(express.json())
 server.use(
-    express.urlencoded({ 
-        extended: true, 
+    express.urlencoded({
+        extended: true,
     })
 )
 
 
-server.use(express.static(path.join(__dirname,"public")))
+server.use(express.static(path.join(__dirname, "public")))
 
-server.get(["/","/index.js"],(req,res) =>{
-    res.render("index",{
-        data,
-    })
+server.get("/", (req, res) => {
+    res.render(path.join("index"))
 })
 
-server.post("/",expressFileUpload(),(req,res) =>{
-   
+
+
+server.post("/",expressFileUpload(),(req,res)=>{
     console.log(req.body);
-    data.push(req.body)
-    res.redirect("/")
-    // console.log(req.body);
+    res.rendirect("/");
 })
 
 
-server.get("/admin", (req,res) =>{
-    
-    res.render("index",{
-        data,
-    })
-})
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// server.get(["/","/index.js"],(req,res) =>{
+//     res.render("index",{
+//         data,
+//     })
+// })
+
+// server.post("/",expressFileUpload(),(req,res) =>{
+//     console.log(req.body);
+//     data.push(req.body)
+//     res.redirect("/")
+//     // console.log(req.body);
+// })
+
+
+// server.get("/admin", (req,res) =>{
+//     res.render("index",{
+//         data,
+//     })
+// })
